c060fb2bb71c8f04a8fbfff3f588d642e87acf8d
Alexander <mburukungu@gmail.com>
2017-04-24 17:40:43 +0300

    second commit

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+// funcion isa piece of code that does one or more actions
+//function accepts arguments and runs muitiple commmands
+
+
+function go(name, age){
+  alert(name);
+  alert(age);
+}
+go("Alexander", 34);
+go("Margee", 28);
